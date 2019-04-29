03a634f3e1ddda6ed5391ebee156ec003a5cf579
colebyk <kauffman.coleby@gmail.com>
2019-04-29 12:48:52 -0600

    added multi-thread capability to run multiple sockets at a time

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -11,24 +11,36 @@ public class ServerMain {
         int port = 8818;
         try {
             ServerSocket serverSocket = new ServerSocket(port);
-            while(true) {
+            while (true) {
                 System.out.println("About to accept client connection...");
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
-                OutputStream outputStream = clientSocket.getOutputStream();
-                for (int i = 0; i< 10; i++) {
-                    outputStream.write(("Time is now " + new Date() + "\n").getBytes());
-                    Thread.sleep(10000);
-                }
-                outputStream.write("Hello World\n Coleby K here\n".getBytes());
-                clientSocket.close();
+                Thread t = new Thread() {
+                    @Override
+                    public void run() {
+                        try {
+                            handleClientSocket(clientSocket);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
+                t.start();
             }
-
-        } catch (IOException e){
-            e.printStackTrace();
-        } catch (InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
+    private static void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
+        OutputStream outputStream = clientSocket.getOutputStream();
+        for (int i = 0; i< 10; i++) {
+            outputStream.write(("Time is now " + new Date() + "\n").getBytes());
+            Thread.sleep(1000);
+        }
+        //outputStream.write("Hello World\n Coleby K here\n".getBytes());
+        clientSocket.close();
     }
 }
