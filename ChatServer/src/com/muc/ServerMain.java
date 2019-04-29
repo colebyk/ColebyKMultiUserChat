63fcd53604686f44598fe2ef8d6526af42ba047d
colebyk <kauffman.coleby@gmail.com>
2019-04-29 12:15:37 -0600

    Prints time every second for ten seconds

--- ChatServer/src/com/muc/ServerMain.java
+++ ChatServer/src/com/muc/ServerMain.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Date;
 
 public class ServerMain {
     public static void main(String[] args) {
@@ -15,11 +16,17 @@ public class ServerMain {
                 Socket clientSocket = serverSocket.accept();
                 System.out.println("Accepted connection from " + clientSocket);
                 OutputStream outputStream = clientSocket.getOutputStream();
+                for (int i = 0; i< 10; i++) {
+                    outputStream.write(("Time is now " + new Date() + "\n").getBytes());
+                    Thread.sleep(1000);
+                }
                 outputStream.write("Hello World\n Coleby K here\n".getBytes());
                 clientSocket.close();
             }
 
         } catch (IOException e){
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
