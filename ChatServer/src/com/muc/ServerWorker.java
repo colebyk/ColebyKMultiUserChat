364dce0be4b43cba9cee3fc0dd729a288164cfbc
colebyk <kauffman.coleby@gmail.com>
2019-05-03 12:56:17 -0600

    Added Server class, login capability, and message that notifies everyone logged in of new logins

--- ChatServer/src/com/muc/ServerWorker.java
+++ ChatServer/src/com/muc/ServerWorker.java
@@ -4,15 +4,18 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.*;
 import java.net.Socket;
-import java.util.Date;
+import java.util.List;
 
 public class ServerWorker extends Thread {
 
     private final Socket clientSocket;
+    private String login = null;
+    private final Server server;
+    private OutputStream outputStream;
 
-    public ServerWorker(Socket clientSocket) {
+    public ServerWorker(Server server, Socket clientSocket) {
+        this.server = server;
         this.clientSocket = clientSocket;
-
     }
 
     @Override
@@ -27,14 +30,19 @@ public class ServerWorker extends Thread {
     }
 
     public void handleClientSocket(Socket clientSocket) throws IOException, InterruptedException {
-        OutputStream outputStream = clientSocket.getOutputStream();
+        this.outputStream = clientSocket.getOutputStream();
         InputStream inputStream = clientSocket.getInputStream();
-        outputStream.write("Enter a message: ".getBytes());
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         String line;
         while ( (line = reader.readLine()) != null ) { // if the line is not empty
-            String[] tokens = StringUtils.split(line);
+
+            /*
+            Problem with StringUtils:
+                downloaded apache module from apache website, implemented the commons-lang3-3.9.jar as a dependency
+                in  File -> Project Structure -> Modules -> Dependencies
+            */
+            String[] tokens = StringUtils.split(line); // split at spaces in string
             if (tokens != null && tokens.length > 0) {
                 String cmd = tokens[0];
                 if ("quit".equalsIgnoreCase(line)) { // equalsIgnoreCase ignores casing--could be "QUIT", "qUiT", etc
@@ -48,10 +56,43 @@ public class ServerWorker extends Thread {
                 }
             }
         }
-
         clientSocket.close();
+    } // end of handleClientSocket method
+
+    public String getLogin() {
+        return login;
     }
 
-    private void handleLogin(OutputStream outputStream, String[] tokens) {
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+        String msg;
+        if (tokens.length == 3) {
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if ( (login.equals("guest") && password.equals("guest"))
+                    || (login.equals("jim") && password.equals("jim")) ) {
+                msg = "ok login\n";
+                outputStream.write(msg.getBytes());
+                this.login = login; // sets class login string to handleLogin's login value
+                System.out.println("User logged in successfully: " + login);
+
+                String onlineMsg = login + " is now online" + "\n";
+
+                List<ServerWorker> workerList = server.getWorkerList();
+                for (ServerWorker worker : workerList) {
+                    worker.send(onlineMsg);
+                }
+
+            } else {
+                msg = "error in login\n";
+                outputStream.write(msg.getBytes());
+            }
+
+        }
+    }  // end of handleLogin method
+
+    private void send(String msg) throws IOException {
+        outputStream.write(msg.getBytes());
+
     }
-}
+} // end of ServerWorker class
